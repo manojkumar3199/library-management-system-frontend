d6e052ba94dca8424c9339eda31f69e8f9febe4e
Manoj Kumar <manojkumarus03@gmail.com>
2023-02-19 14:10:12 +0530

    eighth commit

--- src/app/issuebook/feature/issuebook-shell/issuebook-shell-routing.module.ts
+++ src/app/issuebook/feature/issuebook-shell/issuebook-shell-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'add', loadChildren: () => import('../issuebook-add/issuebook-add.module').then(m => m.IssuebookAddModule) }, { path: 'list', loadChildren: () => import('../issuebook-list/issuebook-list.module').then(m => m.IssuebookListModule) }, { path: 'edit', loadChildren: () => import('../issuebook-edit/issuebook-edit.module').then(m => m.IssuebookEditModule) }, { path: 'detail', loadChildren: () => import('../issuebook-detail/issuebook-detail.module').then(m => m.IssuebookDetailModule) }];
+const routes: Routes = [
+  {
+    path: 'add', loadChildren: () => import('../issuebook-add/issuebook-add.module').then(m => m.IssuebookAddModule)
+  },
+  {
+    path: 'list', loadChildren: () => import('../issuebook-list/issuebook-list.module').then(m => m.IssuebookListModule)
+  },
+  {
+    path: 'detail', loadChildren: () => import('../issuebook-detail/issuebook-detail.module').then(m => m.IssuebookDetailModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
